9b4f7b05837ee2879121bd2e80a8bd91321b261b
mayue666 <38532166+mayue666@users.noreply.github.com>
2018-06-19 14:07:31 +0800

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,60 @@
+    var text = "";
+  }
+  
+var description = document.getElementById("description");
+
+  if (description.firstChild.nodeType == 3) {
+    
+description.firstChild.nodeValue = text;
+  }
+ 
+ return false;
+}
+
+
+function prepareGallery()
+ {
+  if (!document.getElementsByTagName) return false;
+
+  if (!document.getElementById) return false;
+ 
+ if (!document.getElementById("imagegallery")) return false;
+ 
+ var gallery = document.getElementById("imagegallery");
+ 
+ var links = gallery.getElementsByTagName("a");
+  
+for ( var i=0; i < links.length; i++) 
+{
+    links[i].onclick = function() 
+{
+      return showPic(this);
+	}
+   
+ links[i].onkeypress = links[i].onclick;
+  }
+}
+
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  
+if (typeof window.onload != 'function') 
+{
+    window.onload = func;
+  }
+ else 
+{
+    window.onload = function() {
+     
+      oldonload();
+      
+      func();
+
+    }
+
+  }
+
+}
+
+addLoadEvent(prepareGallery);
